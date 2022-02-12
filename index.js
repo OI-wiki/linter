925ca19de208ec1931a3955b963471c190f75679
Ir1d <sirius.caffrey@gmail.com>
2022-02-11 22:51:06 -0600

    feat: support manual lint

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const { exec } = require('child_process');
 let env_variables = 'PATH=' + process.env.PATH;
 
 webhooks.on(['push', 'pull_request.opened', 'pull_request.synchronize'], async ({ id, name, payload }) => {
-  console.log(name, 'event received');
+  console.log(name, 'pr event received');
   const push = payload;
   if (push.pull_request && push.pull_request.title.indexOf('[lint skip]') < 0 && push.sender.login != "24OI-bot") {
     const pr_owner = push.pull_request.head.user.login;
@@ -66,6 +66,52 @@ webhooks.on(['push', 'pull_request.opened', 'pull_request.synchronize'], async (
     console.log(`lint skipped ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
   }
 })
+
+webhooks.on(
+  [
+    "issue_comment.created",
+    "issue_comment",
+    "issue_comment.deleted",
+    "issue_comment.edited",
+  ],
+  async ({ id, name, payload }) => {
+    console.log(name, "issue event received");
+    const comment_body = payload.comment.body;
+    if (comment_body.includes("@24OI-bot") && comment_body.includes("please")) {
+      const api_url = payload.issue.pull_request.url;
+      fetch(api_url)
+        .then((res) => res.text())
+        .then((text) => {
+          const json = JSON.parse(text);
+          const pr_owner = json.head.user.login;
+          const pr_repo = json.head.repo.name;
+          const head_branch = json.head.ref;
+          const pr_number = json.number;
+          console.log(
+            `manual relint starts ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`
+          );
+          exec(
+            env_variables +
+              `bash ./lint.sh ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`,
+            {
+              env: { GH_TOKEN: process.env.GH_TOKEN },
+              uid: 0,
+              maxBuffer: 1024 * 500,
+            },
+            (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+              console.log(
+                `manual relint finishes ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`
+              );
+            }
+          );
+        });
+    }
+  }
+);
 
 require('http').createServer(webhooks.middleware).listen(3000)
 
