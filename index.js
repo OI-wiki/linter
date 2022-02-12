5da8d49cfa64cd6818dda3dfbb0e89ebc6a7d298
Ir1d <sirius.caffrey@gmail.com>
2022-02-11 22:23:10 -0600

    chore: better logging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('log-timestamp');
 const octokit = require('@octokit/rest')()
 const remark = require('remark');
 const rguide = require('remark-preset-lint-markdown-style-guide');
@@ -53,81 +54,17 @@ webhooks.on(['push', 'pull_request.opened', 'pull_request.synchronize'], async (
     const pr_repo = push.pull_request.head.repo.name;
     const head_branch = push.pull_request.head.ref;
     const pr_number = push.number;
-    console.log('lint starts');
+    console.log(`lint starts ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
     exec(env_variables + `bash ./lint.sh ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`, { env: { 'GH_TOKEN': process.env.GH_TOKEN }, uid: 0, maxBuffer: 1024 * 500}, (error, stdout, stderr) => {
-      // console.log(`stdout: ${stdout}`);
-      // console.log(`stderr: ${stderr}`);
       if(error) {
         console.error(`exec error: ${error}`);
         return;
       }
-      console.log('lint finishes');
+      console.log(`lint finishes ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
     });
   } else {
-    console.log('lint skipped');
+    console.log(`lint skipped ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
   }
-
-  // const compare = await octokit.repos.compareCommits({
-  //   owner: push.pull_request.base.user.login,
-  //   repo: push.pull_request.base.repo.name,
-  //   base: push.pull_request.base.sha,
-  //   head: push.pull_request.head.sha
-  // })
-
-  // compare.data.files.map(async file => {
-  //   if (file.filename.endsWith('.md')) {
-  //     const content = await octokit.repos.getContent({
-  //       path: file.filename,
-  //       owner: push.pull_request.head.user.login,
-  //       repo: push.pull_request.head.repo.name
-  //     })
-  //     const text = Buffer.from(content.data.content, 'base64').toString()
-  //     // console.log(typeof text)
-  //     // console.log(typeof rmath)
-  //     console.log(file.filename);
-  //     myremark.process(text, (err, outputs) => {
-  //       if (err) {
-  //         throw new Error(err)
-  //       }
-  //       // console.log(outputs);
-  //       console.log({
-  //         owner: push.pull_request.head.user.login,
-  //         repo: push.pull_request.head.repo.name,
-  //         path: file.filename,
-  //         message: `style: fix lint errors for ${file.filename}`,
-  //         content: outputs.toString('base64'),
-  //         sha: content.data.sha,
-  //         branch: head_branch,
-  //         author: {
-  //           name: '24OI-bot',
-  //           email: '15963390+24OI-bot@users.noreply.github.com'
-  //         }
-  //       })
-  //       return Promise.all([outputs].map(output => {
-  //         // console.log('??' + output);
-  //         // console.log(typeof output);
-  //         octokit.repos.updateFile({
-  //           owner: push.pull_request.head.user.login,
-  //           repo: push.pull_request.head.repo.name,
-  //           path: file.filename,
-  //           message: `style: fix lint errors for ${file.filename}`,
-  //           content: output.toString('base64'),
-  //           sha: content.data.sha,
-  //           branch: head_branch,
-  //           author: {
-  //             name: '24OI-bot',
-  //             email: '15963390+24OI-bot@users.noreply.github.com'
-  //           }
-  //         }, (err, res) => {
-  //           if (err) {
-  //             throw new Error(err)
-  //           }
-  //           console.log(res);
-  //         })
-  //       }))
-  //     })
-  //   }
-  // })
 })
 
 require('http').createServer(webhooks.middleware).listen(3000)
