341dc1eb4c4d914a35909fd764ec8b14ec4a5454
mgt <mgt@oi-wiki.org>
2024-05-07 22:33:21 +0800

    refactor: make code better

--- index.js
+++ index.js
@@ -30,6 +30,38 @@ webhooks.onError((error) => {
 
 
 const { exec } = require('child_process');
+const asyncExec = require('util').promisify(exec);
+
+async function approveWithComment(owner, repo, number, comment) {
+  await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
+    owner: owner,
+    repo: repo,
+    pull_number: number,
+    body: comment,
+    event: 'APPROVE'
+  })
+}
+
+async function postReactions(owner, repo, comment_id, content) {
+  await octokit.request('POST /repos/{owner}/{repo}/issues/comments/{comment_id}/reactions', {
+    owner: owner,
+    repo: repo,
+    comment_id: comment_id,
+    content: content
+  })
+}
+
+async function execLint(owner, repo, branch, number) {
+  try {
+    await asyncExec(`bash ./lint.sh ${owner} ${repo} ${branch} ${number}`, { env: { 'GH_TOKEN': process.env.GH_TOKEN }, uid: 0, maxBuffer: 1024 * 500 });
+    console.log(`lint finishes for ${owner}/${repo}#${number}`);
+    await approveWithComment(owner, repo, number, 'Lint finished, ready for review :)');
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
 
 let env_variables = 'PATH=' + process.env.PATH;
 
@@ -45,34 +77,11 @@ webhooks.on(['push', 'pull_request.opened', 'pull_request.synchronize', 'pull_re
     const head_branch = push.pull_request.head.ref;
     const pr_number = push.number;
     console.log(`lint starts ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
-    exec(env_variables + `bash ./lint.sh ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`, { env: { 'GH_TOKEN': process.env.GH_TOKEN }, uid: 0, maxBuffer: 1024 * 500 }, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`lint finishes ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`);
-    });
-    try {
-      await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
-        owner: 'OI-wiki',
-        repo: 'OI-wiki',
-        pull_number: pr_number,
-        body: 'Lint finished, ready for review :)',
-        event: 'APPROVE'
-      })
-    } catch (err) {
-      console.error(err)
-    }
+    await execLint(pr_owner, pr_repo, head_branch, pr_number)
   } else {
     console.log(`lint skipped`);
     try {
-      await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/reviews', {
-        owner: 'OI-wiki',
-        repo: 'OI-wiki',
-        pull_number: pr_number,
-        body: 'Lint skipped, unhappy :(',
-        event: 'APPROVE'
-      })
+      await approveWithComment('OI-wiki', 'OI-wiki', push.number, 'Lint skipped, unhappy :(');
     } catch (err) {
       console.error(err)
     }
@@ -100,56 +109,28 @@ webhooks.on(
       } catch (err) {
         console.error(err)
       }
-      fetch(api_url)
-        .then((res) => res.text())
-        .then((text) => {
-          const json = JSON.parse(text);
-          const pr_owner = json.head.user.login;
-          const pr_repo = json.head.repo.name;
-          const head_branch = json.head.ref;
-          const pr_number = json.number;
-          console.log(
-            `manual relint starts ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`
-          );
-          exec(
-            env_variables +
-            `bash ./lint.sh ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`,
-            {
-              env: { GH_TOKEN: process.env.GH_TOKEN },
-              uid: 0,
-              maxBuffer: 1024 * 500,
-            },
-            async (error, stdout, stderr) => {
-              if (error) {
-                console.error(`exec error: ${error}`);
-                try {
-                  await octokit.request('POST /repos/{owner}/{repo}/issues/comments/{comment_id}/reactions', {
-                    owner: 'OI-wiki',
-                    repo: 'OI-wiki',
-                    comment_id: payload.comment.id,
-                    content: 'confused'
-                  })
-                } catch (err) {
-                  console.error(err)
-                }
-                return;
-              }
-              console.log(
-                `manual relint finishes ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`
-              );
-              try {
-                await octokit.request('POST /repos/{owner}/{repo}/issues/comments/{comment_id}/reactions', {
-                  owner: 'OI-wiki',
-                  repo: 'OI-wiki',
-                  comment_id: payload.comment.id,
-                  content: '+1'
-                })
-              } catch (err) {
-                console.error(err)
-              }
-            }
-          );
-        });
+      const text = await fetch(api_url).then((res) => res.text());
+      const json = JSON.parse(text);
+      const pr_owner = json.head.user.login;
+      const pr_repo = json.head.repo.name;
+      const head_branch = json.head.ref;
+      const pr_number = json.number;
+      console.log(
+        `manual relint starts ${pr_owner} ${pr_repo} ${head_branch} ${pr_number}`
+      );
+      try {
+        postReactions('OI-wiki', 'OI-wiki', payload.comment.id, 'confused');
+      } catch (err) {
+        console.error(err)
+      }
+      const success = await execLint(pr_owner, pr_repo, head_branch, pr_number);
+      if (success) {
+        try {
+          postReactions('OI-wiki', 'OI-wiki', payload.comment.id, '+1');
+        } catch (err) {
+          console.error(err)
+        }
+      }
     } else {
       console.log('issue event skipped')
     }
